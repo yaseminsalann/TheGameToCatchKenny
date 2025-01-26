008ea0c97ac704e287f002359afd3fd21904ba74
yaseminsalann <yaseminsalann@gmail.com>
2025-01-26 22:38:29 +0300

    kenny yakalama oyunu
    basit bir oyun örneği yapıldı

---
+++ TheGameToCatchKenny/TheGameToCatchKennyTests/TheGameToCatchKennyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheGameToCatchKennyTests.swift
+//  TheGameToCatchKennyTests
+//
+//  Created by Yasemin salan on 26.01.2025.
+//
+
+import Testing
+@testable import TheGameToCatchKenny
+
+struct TheGameToCatchKennyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
